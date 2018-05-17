76474bfcce0291e9ecfab25058a7fb6a910098fe
Walter <walterinsh@icloud.com>
2018-05-17 16:59:19 +0800

    two sum

---
+++ two-sum/force-solution.js
@@ -0,0 +1,9 @@
+var twoSum = function(nums, target) {
+    for(var i = 0; i< nums.length;i++){
+        for(var k = i + 1; k< nums.length;k++){
+            if(nums[i] + nums[k] == target){
+               return [i, k];
+            }
+        }
+    }
+};
